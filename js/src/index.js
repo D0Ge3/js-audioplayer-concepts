31c6b16ea702b8d4234d8efa77f5749170c77f29
Nikita Fedorov <fnik340@gmail.com>
2021-06-30 22:03:15 +0300

    fixes

--- js/src/index.js
+++ js/src/index.js
@@ -1,33 +1,7 @@
 import page from 'page'
-// import '@/styles/index.scss'
 import './styles/index.scss'
 
-
-
-const rootDiv = document.querySelector('#root')
-const parser = new DOMParser()
-const loadContent = (uri) => {
-  fetch(uri)
-    .then((response) => response.text())
-    .then((data) => {
-      const htmlDoc = parser.parseFromString(data, 'text/html')
-
-      rootDiv.innerHTML = htmlDoc.querySelector('#root').innerHTML
-    })
-}
-page('/', () => {
-  loadContent('./index.html')
-})
-page('/artist', () => {
-  loadContent('./artist.html')
-})
-page('/about', () => {
-  loadContent('./about.html')
-})
-page()
-
-
-
+let showPlayerBtn
 class Player {
   constructor({ audioEl, playerEl, btnPlay, btnPause, btnPrev, btnNext, btnVolMinus, btnVolPlus, thumb, playlist }) {
     this.audioEl = audioEl
@@ -43,11 +17,8 @@ class Player {
   }
   curentTrackInd = 0
   switchTrack(numTrack) {
-      // Меняем значение атрибута src
       this.audioEl.src = this.playlist[numTrack]
-      // Назначаем время песни ноль
       this.audioEl.currentTime = 0
-      // Включаем песню
       this.audioEl.play()
     }
   play() {
@@ -57,29 +28,28 @@ class Player {
     this.btnPause.className += "audio-btn audio-btn__pause audio-btn__pause--show"
     this.btnPlay.className = "audio-btn audio-btn__play audio-btn__play--hide"
     this.audioPlay = setInterval(() => {
-      // Получаем значение на какой секунде песня
       const audioTime = Math.round(this.audioEl.currentTime)
-      // Получаем всё время песни
       const audioLength = Math.round(this.audioEl.duration)
-      // Назначаем ширину элементу time
       this.thumb.style.width = (audioTime * 100) / audioLength + '%'
-      // Сравниваем, на какой секунде сейчас трек и всего сколько времени длится
-      // И проверяем что переменная treck меньше четырёх
-      if (audioTime == audioLength && treck < 3) {
-        treck++ // То Увеличиваем переменную 
-        switchTrack(treck) // Меняем трек
-        // Иначе проверяем тоже самое, но переменная treck больше или равна четырём
-      } else if (audioTime == audioLength && treck >= 3) {
-        treck = 0 // То присваиваем treck ноль
-        switchTrack(treck) // Меняем трек
+      if (audioTime == audioLength && this.curentTrackInd < this.playlist.length - 1) {
+        this.curentTrackInd++
+        this.switchTrack(this.curentTrackInd)
+      } else if (audioTime == audioLength && this.curentTrackInd >= this.playlist.length - 1) {
+        this.curentTrackInd = 0
+        this.switchTrack(this.curentTrackInd)
       }
     }, 10)
   }
   pause() {
-      this.audioEl.pause() // Останавливает песню
-      clearInterval(this.audioPlay) // Останавливает интервал
+      this.audioEl.pause()
+      clearInterval(this.audioPlay)
       this.btnPlay.className = "audio-btn audio-btn__play audio-btn__play"
       this.btnPause.className += "audio-btn audio-btn__pause audio-btn__pause"
+  }
+  stop() {
+    this.audioEl.pause()
+    this.audioEl.currentTime = 0
+    clearInterval(this.audioPlay)
   }
   changeVol(mode) {
     if (mode === "minus") {
@@ -93,22 +63,21 @@ class Player {
     }
   }
   nextTrack() {
-      // Проверяем что переменная treck больше трёх
-    if (this.curentTrackInd < this.playlist.length) { // Если да, то
-      this.curentTrackInd++ // Увеличиваем её на один
-      this.switchTrack(this.curentTrackInd) // Меняем песню 
-    } else { // Иначе
-      this.curentTrackInd = 0 // Присваиваем ей ноль
-      this.switchTrack(this.curentTrackInd) // Меняем песню
+    if (this.curentTrackInd < this.playlist.length - 1) {
+      this.curentTrackInd++
+      this.switchTrack(this.curentTrackInd) 
+    } else {
+      this.curentTrackInd = 0
+      this.switchTrack(this.curentTrackInd)
     }
   }
   prevTrack() {
     if (this.curentTrackInd > 0) {
-      this.curentTrackInd-- // Если верно, то уменьшаем переменную на один
-      this.switchTrack(this.curentTrackInd) // Меняем песню.
-    } else { // Иначе
-      this.curentTrackInd = 3 // Присваиваем три
-      this.switchTrack(this.curentTrackInd) // Меняем песню
+      this.curentTrackInd--
+      this.switchTrack(this.curentTrackInd)
+    } else {
+      this.curentTrackInd = this.playlist.length - 1
+      this.switchTrack(this.curentTrackInd)
     }
   }
   init() {
@@ -128,11 +97,51 @@ class Player {
 
 window.onload = () => {
 
-  let playlist = [
-    'https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3',
+  const rootDiv = document.querySelector('#root')
+  const parser = new DOMParser()
+  const loadContent = (uri) => {
+    return fetch(uri)
+      .then((response) => response.text())
+      .then((data) => {
+        const htmlDoc = parser.parseFromString(data, 'text/html')
+
+        rootDiv.innerHTML = htmlDoc.querySelector('#root').innerHTML
+      })
+  }
+  page('/', () => {
+    loadContent('./index.html').then(() => {
+      addListenersToHeader()
+    })
+  })
+  page('/artist', () => {
+    loadContent('./artist.html').then(() => {
+      const tracksPlayBtns = document.querySelectorAll("#play-pause-track")
+      const allTracks = Array.from(tracksPlayBtns).map((trackPlayBtn) => trackPlayBtn.parentElement.getAttribute("data-url"))
+      addListenersToHeader()
+      tracksPlayBtns.forEach((trackPlayBtn, ind) => 
+        trackPlayBtn.addEventListener("click", () => {
+          playerInstance.pause()
+          if (playerInstance.playerEl.className !== "player") {
+            playerInstance.init()
+          }
+          playerInstance.playlist = allTracks
+          playerInstance.curentTrackInd = ind
+          playerInstance.play()
+        }))
+    })
+  
+  })
+  page('/about', () => {
+    loadContent('./about.html').then(() => {
+      addListenersToHeader()
+    })
+  })
+  page()
+
+  let defaultPlaylist = [
+    'https://rss.art19.com/episodes/858792d0-856a-42a3-a8f6-f1590ae44616.mp3?rss_browser=BAhJIglKYXZhBjoGRVQ%3D--631732772f14ff418ee41471e1d52549d5878e4a',
     'https://rss.art19.com/episodes/b243d284-1b4f-4b15-99f1-e2a5d1fe8c86.mp3?rss_browser=BAhJIglKYXZhBjoGRVQ%3D--631732772f14ff418ee41471e1d52549d5878e4a',
-    'https://rss.art19.com/episodes/2106301c-0a16-40a5-91e5-ab428cdbbe5a.mp3',
-    'https://rss.art19.com/episodes/e6527fc1-cf49-438a-af03-032d48d09251.mp3'
+    'https://rss.art19.com/episodes/2106301c-0a16-40a5-91e5-ab428cdbbe5a.mp3'
   ]
 
   const audio = document.querySelector("#audio")
@@ -144,122 +153,14 @@ window.onload = () => {
   const btnVolMinus = document.querySelector("#volume__minus")
   const btnVolPlus = document.querySelector("#volume__plus")
 
-
   const player = document.querySelector("#player")
 
-  const showPlayerBtn = document.querySelector("#show-player")
-  const playerInstance = new Player({audioEl: audio, playerEl: player, btnPlay, btnPause, btnPrev, btnNext, btnVolMinus, btnVolPlus, thumb, playlist })
-  showPlayerBtn.addEventListener("click", () => playerInstance.init());
-  //console.log(playerInstance);
+  const playerInstance = new Player({audioEl: audio, playerEl: player, btnPlay, btnPause, btnPrev, btnNext, btnVolMinus, btnVolPlus, thumb, playlist: defaultPlaylist })
+
+  const addListenersToHeader = () => {
+    showPlayerBtn = document.querySelector("#show-player")
+    showPlayerBtn.addEventListener("click", () => {
+      playerInstance.init()
+    });
+  }
 }
-
-// let audioPlay
-
-// let playlist = [
-//   'https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3',
-//   'https://rss.art19.com/episodes/b243d284-1b4f-4b15-99f1-e2a5d1fe8c86.mp3?rss_browser=BAhJIglKYXZhBjoGRVQ%3D--631732772f14ff418ee41471e1d52549d5878e4a',
-//   'https://rss.art19.com/episodes/2106301c-0a16-40a5-91e5-ab428cdbbe5a.mp3',
-//   'https://rss.art19.com/episodes/e6527fc1-cf49-438a-af03-032d48d09251.mp3'
-// ]
-
-// let treck // Переменная с индексом трека
-
-// const playAudio = () => {
-//   audio.play() // Запуск песни
-//   btnPause.className += "audio-btn audio-btn__pause audio-btn__pause--show"
-//   btnPlay.className = "audio-btn audio-btn__play audio-btn__play--hide"
-//   // Запуск интервала 
-//   audioPlay = setInterval(() => {
-//     // Получаем значение на какой секунде песня
-//     const audioTime = Math.round(audio.currentTime)
-//     // Получаем всё время песни
-//     const audioLength = Math.round(audio.duration)
-//     // Назначаем ширину элементу time
-//     thumb.style.width = (audioTime * 100) / audioLength + '%'
-//     // Сравниваем, на какой секунде сейчас трек и всего сколько времени длится
-//     // И проверяем что переменная treck меньше четырёх
-//     if (audioTime == audioLength && treck < 3) {
-//       treck++ // То Увеличиваем переменную 
-//       switchTreck(treck) // Меняем трек
-//       // Иначе проверяем тоже самое, но переменная treck больше или равна четырём
-//     } else if (audioTime == audioLength && treck >= 3) {
-//       treck = 0 // То присваиваем treck ноль
-//       switchTreck(treck) // Меняем трек
-//     }
-//   }, 10)
-// }
-
-// // Событие перед загрузкой страницы
-// window.onload = function() {
-//   treck = 0 // Присваиваем переменной ноль
-//   const showPlayerBtn = document.querySelector("#show-player")
-//   showPlayerBtn.addEventListener("click", () => {
-//     player.className = "player"
-//   })
-
-//   const tracksPlayBtns = document.querySelectorAll("#play-pause-track")
-//   const allTracks = Array.from(tracksPlayBtns).map((trackPlayBtn) => trackPlayBtn.parentElement.getAttribute("data-url"))
-
-//   tracksPlayBtns.forEach((trackPlayBtn) => 
-//     trackPlayBtn.addEventListener("click", () => {
-//       const currTrackInd = allTracks.indexOf(trackPlayBtn.parentElement.getAttribute("data-url"))
-//       playlist = allTracks.filter((val, ind) => ind >= currTrackInd)
-//       player.className = "player"
-//       playAudio()
-//     })
-//   )
-// }
-
-// function switchTreck (numTreck) {
-//   // Меняем значение атрибута src
-//   audio.src = playlist[numTreck]
-//   // Назначаем время песни ноль
-//   audio.currentTime = 0
-//   // Включаем песню
-//   audio.play()
-// }
-
-
-
-// btnPlay.addEventListener("click", () => playAudio)
-
-// btnPause.addEventListener("click", () => {
-//   audio.pause() // Останавливает песню
-//   clearInterval(audioPlay) // Останавливает интервал
-//   btnPlay.className = "audio-btn audio-btn__play audio-btn__play"
-//   btnPause.className += "audio-btn audio-btn__pause audio-btn__pause"
-// })
-
-// btnPrev.addEventListener("click", () => {
-//   // Проверяем что переменная treck больше нуля
-//   if (treck > 0) {
-//     treck-- // Если верно, то уменьшаем переменную на один
-//     switchTreck(treck) // Меняем песню.
-//   } else { // Иначе
-//     treck = 3 // Присваиваем три
-//     switchTreck(treck) // Меняем песню
-//   }
-// })
-
-// btnNext.addEventListener("click", () => {
-//   // Проверяем что переменная treck больше трёх
-//   if (treck < 3) { // Если да, то
-//     treck++ // Увеличиваем её на один
-//     switchTreck(treck) // Меняем песню 
-//   } else { // Иначе
-//     treck = 0 // Присваиваем ей ноль
-//     switchTreck(treck) // Меняем песню
-//   }
-// })
-
-// btnVolPlus.addEventListener("click", () => {
-//   if (audio.volume < 1) {
-//     audio.volume += 0.1
-//   }
-// })
-
-// btnVolMinus.addEventListener("click", () => {
-//   if (audio.volume > 0.1) {
-//     audio.volume -= 0.1
-//   }
-// })
